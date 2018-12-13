5b5769acb54e6ed6d66b68d6499a54ee82711292
Pavel Durandin <avorio@mail.ru>
2018-12-13 19:08:36 +0700

    BaseURL fixed

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const path = require('path')
 
 module.exports = {
   outputDir: path.resolve(__dirname, './docs'),
-  baseUrl: process.env.NODE_ENV === 'production' ? '/asia-vietnam.ru/' : '/'
+  baseUrl: '/'
 }
