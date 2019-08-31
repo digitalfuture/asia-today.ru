f245e0115ec424a588f94b6d0b58b2b6d5464044
digitalfuture <avorio@mail.ru>
2019-09-01 04:26:19 +0800

    New build

--- vue.config.js
+++ vue.config.js
@@ -6,27 +6,5 @@ module.exports = {
   configureWebpack: {
     devtool: 'source-map',
     mode: 'production'
-  },
-  pwa: {
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    iconPaths: {
-      favicon512: 'icons/icon-512x512',
-      favicon384: 'icons/icon-384x384',
-      favicon192: 'icons/icon-192x192',
-      favicon152: 'icons/icon-152x152',
-      favicon144: 'icons/icon-144x144',
-      favicon128: 'icons/icon-128x128'
-    },
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js'
-      // ...other Workbox options...
-    }
   }
 }
