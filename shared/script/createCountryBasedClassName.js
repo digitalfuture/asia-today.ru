e140f80924f667ed99ca1e535ee63b1352b5a46d
digitalfuture <avorio@mail.ru>
2019-09-26 14:35:39 +0800

    Fixed

--- shared/script/createCountryBasedClassName.js
+++ shared/script/createCountryBasedClassName.js
@@ -3,7 +3,7 @@ import { countries } from './config.json'
 // Add country-based class
 export default function createCountryBasedClassName() {
   for (const country of countries) {
-    if (window.location.href.indexOf(country) === -1) continue
-    document.querySelector('body').classList.add(country)
+    if (window.location.href.indexOf(country.name) === -1) continue
+    document.querySelector('body').classList.add(country.name)
   }
 }
