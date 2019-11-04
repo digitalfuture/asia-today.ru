b515ba6cfb4d5510c54e12b59ab62255dd962294
digitalfuture <avorio@mail.ru>
2019-11-04 20:50:35 +0800

    New build

---
+++ docs/precache-manifest.4a43456b4c0b6d8eb3879fc1f77de3ba.js
@@ -0,0 +1,366 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85aec4f85c23b287f1782026c3f8e4d0",
+    "url": "/404.html"
+  },
+  {
+    "revision": "1c82737a27df53663a8f",
+    "url": "/css/app.6a619576.css"
+  },
+  {
+    "revision": "3d3fc95f3062a7b86664",
+    "url": "/css/chunk-vendors.31599eb0.css"
+  },
+  {
+    "revision": "4cb555b1e9432bfbf76a1abf7d10d96a",
+    "url": "/favicon-today.png"
+  },
+  {
+    "revision": "dccd947b8d5cf4290079da3b943135c9",
+    "url": "/img/background-footer.png"
+  },
+  {
+    "revision": "3311e71043774475252bdc3a2c70d3c0",
+    "url": "/img/logo-asia-icon.png"
+  },
+  {
+    "revision": "777f7a350ff4c42f0c36081fea0bad55",
+    "url": "/img/logo-today-transparent.png"
+  },
+  {
+    "revision": "267765971aadf06db9c42d2e346b85ae",
+    "url": "/img/placeholder.jpg"
+  },
+  {
+    "revision": "1f4d4763478aff60e0c245176dc1ec2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c82737a27df53663a8f",
+    "url": "/js/app.e0adfee6.js"
+  },
+  {
+    "revision": "3d3fc95f3062a7b86664",
+    "url": "/js/chunk-vendors.920e7fab.js"
+  },
+  {
+    "revision": "05c3fa36c48fefe50eb7d33d1f3f0d46",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7f4fe24dd7640c33110cd81476248617",
+    "url": "/shared/css/style.css"
+  },
+  {
+    "revision": "273ef66d7e87e717ace137461c66af0e",
+    "url": "/shared/img/logo-cambodia-bold.png"
+  },
+  {
+    "revision": "571148975b719f3b7ac6006836aad90d",
+    "url": "/shared/img/logo-cambodia-icon.png"
+  },
+  {
+    "revision": "bbda6f81fc17dca443d2327dddee51c6",
+    "url": "/shared/img/logo-cambodia-white-bold.png"
+  },
+  {
+    "revision": "cf0ea19b3a34b54d6a5258647ab7961b",
+    "url": "/shared/img/logo-cambodia-white.png"
+  },
+  {
+    "revision": "2e9f85033bc01d76ff589ea8646bf27b",
+    "url": "/shared/img/logo-cambodia.png"
+  },
+  {
+    "revision": "9d566c204e72692d9cf3e8a66a828a14",
+    "url": "/shared/img/logo-china-bold.png"
+  },
+  {
+    "revision": "74fe6d1c87c73e787bb39b477bbf79e4",
+    "url": "/shared/img/logo-china-icon.png"
+  },
+  {
+    "revision": "464d4141a2e80d8e977d97158e5b19ca",
+    "url": "/shared/img/logo-china-white-bold.png"
+  },
+  {
+    "revision": "2302486d54f2cabfa0a53ba0663d6978",
+    "url": "/shared/img/logo-china-white.png"
+  },
+  {
+    "revision": "c596515f421ab167e859cef96d5baeef",
+    "url": "/shared/img/logo-china.png"
+  },
+  {
+    "revision": "e2313020550fc5a1291726277810e98c",
+    "url": "/shared/img/logo-india-bold.png"
+  },
+  {
+    "revision": "ef9f3a61a507b28163ea2b67fdd6aece",
+    "url": "/shared/img/logo-india-icon.png"
+  },
+  {
+    "revision": "1cb72cec509afc184d044d7dd1f4d4f8",
+    "url": "/shared/img/logo-india-white-bold.png"
+  },
+  {
+    "revision": "3d35b86c4994b307339362919ae9f342",
+    "url": "/shared/img/logo-india-white.png"
+  },
+  {
+    "revision": "c0145480f512a24d9fc1eed6face55b8",
+    "url": "/shared/img/logo-india.png"
+  },
+  {
+    "revision": "8511a9f02fc990d6cd752c800a50c5fd",
+    "url": "/shared/img/logo-japan-bold.png"
+  },
+  {
+    "revision": "3748c130bfc0fce851fd954944597716",
+    "url": "/shared/img/logo-japan-icon.png"
+  },
+  {
+    "revision": "6956e9354c06228a56fcbcdee816b77c",
+    "url": "/shared/img/logo-japan-white-bold.png"
+  },
+  {
+    "revision": "7e87eced52a18c22e0894edb0d368c66",
+    "url": "/shared/img/logo-japan-white.png"
+  },
+  {
+    "revision": "b8cfd3f8191c257452d39f9159fc7cee",
+    "url": "/shared/img/logo-japan.png"
+  },
+  {
+    "revision": "e234e60efe510113b4e14b16685c5b37",
+    "url": "/shared/img/logo-korea-bold.png"
+  },
+  {
+    "revision": "1a4ba90129eb7d0b235d396f1a91eed2",
+    "url": "/shared/img/logo-korea-icon.png"
+  },
+  {
+    "revision": "5ce569c847fca80720bfe604d7867d3e",
+    "url": "/shared/img/logo-korea-white-bold.png"
+  },
+  {
+    "revision": "8afe1015c246c497ec7650450f598f53",
+    "url": "/shared/img/logo-korea-white.png"
+  },
+  {
+    "revision": "3b088845405a1f83aba3de2af1aaaf6a",
+    "url": "/shared/img/logo-korea.png"
+  },
+  {
+    "revision": "274291ae68c329bd3a83080628f73691",
+    "url": "/shared/img/logo-laos-bold.png"
+  },
+  {
+    "revision": "5da1562de9ed5636f0461715f6114690",
+    "url": "/shared/img/logo-laos-icon.png"
+  },
+  {
+    "revision": "45c585e931a2e0aae799352d4141f1be",
+    "url": "/shared/img/logo-laos-white-bold.png"
+  },
+  {
+    "revision": "d6680b6f59f1775440075fbdd68ed375",
+    "url": "/shared/img/logo-laos.png"
+  },
+  {
+    "revision": "942fe99b6b7d852ac1a37612ff3ea306",
+    "url": "/shared/img/logo-malaysia-bold.png"
+  },
+  {
+    "revision": "cede52e229ab0f75eab87a845d36e68c",
+    "url": "/shared/img/logo-malaysia-icon.png"
+  },
+  {
+    "revision": "8f63a7098dc4de2bad3f19ab9b98b9c0",
+    "url": "/shared/img/logo-malaysia-white-bold.png"
+  },
+  {
+    "revision": "54e9f7e3f28a54cde684f625c3f46d2c",
+    "url": "/shared/img/logo-malaysia-white.png"
+  },
+  {
+    "revision": "cc7c8883f553a635392c332d60f8afd8",
+    "url": "/shared/img/logo-malaysia.png"
+  },
+  {
+    "revision": "560e5f949503b1e8edd2e0c29a3005ea",
+    "url": "/shared/img/logo-myanmar-bold.png"
+  },
+  {
+    "revision": "344e53dd94d3028b29e62ee853f1609b",
+    "url": "/shared/img/logo-myanmar-icon.png"
+  },
+  {
+    "revision": "b668e30e0587b87951e15b7a51871a45",
+    "url": "/shared/img/logo-myanmar-white-bold.png"
+  },
+  {
+    "revision": "8516ab8a7770203a4bf523325a57165c",
+    "url": "/shared/img/logo-myanmar-white.png"
+  },
+  {
+    "revision": "25693e9280f018c0e4a8081af2f249a9",
+    "url": "/shared/img/logo-myanmar.png"
+  },
+  {
+    "revision": "684361ad43b31a9a19329f02947f1975",
+    "url": "/shared/img/logo-nepal-bold.png"
+  },
+  {
+    "revision": "e0abeb3869a5fefc311517fbad46d260",
+    "url": "/shared/img/logo-nepal-icon.png"
+  },
+  {
+    "revision": "2923f5e04bfa0fa8d69fac96552b3e96",
+    "url": "/shared/img/logo-nepal-white-bold.png"
+  },
+  {
+    "revision": "ea6e7665268cf7285a582227f281f9e6",
+    "url": "/shared/img/logo-nepal-white.png"
+  },
+  {
+    "revision": "292425004601669e9f4d4a069d40baba",
+    "url": "/shared/img/logo-nepal.png"
+  },
+  {
+    "revision": "881ca1e4bb965c6c466388b21ec5f55d",
+    "url": "/shared/img/logo-philippines-bold.png"
+  },
+  {
+    "revision": "d35b4bb527ae1299b47ad5c8c5008a02",
+    "url": "/shared/img/logo-philippines-icon.png"
+  },
+  {
+    "revision": "a2c36d72542eccb92115e0398e7ae772",
+    "url": "/shared/img/logo-philippines-white-bold.png"
+  },
+  {
+    "revision": "e9f7c5c6236ff36558a77be88b9a9aee",
+    "url": "/shared/img/logo-philippines-white.png"
+  },
+  {
+    "revision": "50f635073b13411c3131623444dec9c9",
+    "url": "/shared/img/logo-philippines.png"
+  },
+  {
+    "revision": "01f5c0d52493d39020094f8d860cfed1",
+    "url": "/shared/img/logo-singapore-bold.png"
+  },
+  {
+    "revision": "389ad067696b6d42cd38b42d43b7d480",
+    "url": "/shared/img/logo-singapore-icon.png"
+  },
+  {
+    "revision": "aee600c062207585c71eedc13751fd01",
+    "url": "/shared/img/logo-singapore-white-bold.png"
+  },
+  {
+    "revision": "790c946d7617febefc516103088d8568",
+    "url": "/shared/img/logo-singapore-white.png"
+  },
+  {
+    "revision": "c6bdcc2c9d1a78ee7f79fdd9539a8960",
+    "url": "/shared/img/logo-singapore.png"
+  },
+  {
+    "revision": "85999e536acf343a30e834a1a08a1e58",
+    "url": "/shared/img/logo-sri-lanka-bold.png"
+  },
+  {
+    "revision": "3c1ae8b65d3c2ec0fdfca036d22c3377",
+    "url": "/shared/img/logo-sri-lanka-icon.png"
+  },
+  {
+    "revision": "bf0fb9cd835b844ee01534feb77a37e2",
+    "url": "/shared/img/logo-sri-lanka-white-bold.png"
+  },
+  {
+    "revision": "3c0873e09ede428e12a924e4091c8046",
+    "url": "/shared/img/logo-sri-lanka-white.png"
+  },
+  {
+    "revision": "8ffef5c6870b584868811680720283d3",
+    "url": "/shared/img/logo-sri-lanka.png"
+  },
+  {
+    "revision": "2121766086cd9e437c5aa35847f888ef",
+    "url": "/shared/img/logo-thailand-bold.png"
+  },
+  {
+    "revision": "602cfde1c23b50e33aee5602b7a32f5a",
+    "url": "/shared/img/logo-thailand-icon.png"
+  },
+  {
+    "revision": "0d810738fb1a554cc6b879ba1c4ceda5",
+    "url": "/shared/img/logo-thailand-white-bold.png"
+  },
+  {
+    "revision": "fa479dfc23d46c399e9db8aa9932bf91",
+    "url": "/shared/img/logo-thailand-white.png"
+  },
+  {
+    "revision": "16bba77266edc27df3a61c21952a6f85",
+    "url": "/shared/img/logo-thailand.png"
+  },
+  {
+    "revision": "1368337c192717c8cb574f520a802e6f",
+    "url": "/shared/img/logo-today-transparent.png"
+  },
+  {
+    "revision": "2e0f53ab1218a7061b3c0cdfc3eadf2c",
+    "url": "/shared/img/logo-vietnam-bold.png"
+  },
+  {
+    "revision": "8b0db3a00f9cce4b8bad5c8a3354ba3d",
+    "url": "/shared/img/logo-vietnam-icon.png"
+  },
+  {
+    "revision": "1de3c1d203450839049aaaf1748d57f0",
+    "url": "/shared/img/logo-vietnam-white-bold.png"
+  },
+  {
+    "revision": "110b97ec135d48448e44518d0db26d0b",
+    "url": "/shared/img/logo-vietnam-white.png"
+  },
+  {
+    "revision": "53e06ca404193839fa4a3ba65bda4e2d",
+    "url": "/shared/img/logo-vietnam.png"
+  },
+  {
+    "revision": "d87052eae0b4f20759c344638ffcb64f",
+    "url": "/shared/img/social/ok-orange.png"
+  },
+  {
+    "revision": "de52b3ac9a1b2b86c967cdd8d15de13f",
+    "url": "/shared/img/social/ok-white.png"
+  },
+  {
+    "revision": "3b8ad4362154b54cd4e5046b41489eec",
+    "url": "/shared/img/social/zen.svg"
+  },
+  {
+    "revision": "314ec3173deee24811b22a97c6dbe64e",
+    "url": "/shared/script/components/country-list.js"
+  },
+  {
+    "revision": "d1d8c9dac291df880a0d55f2672e2e52",
+    "url": "/shared/script/components/country-list.min.js"
+  },
+  {
+    "revision": "1bece9a38c3c250041a45f6005fdd9df",
+    "url": "/shared/script/components/demo.html"
+  },
+  {
+    "revision": "756c5e71fa9b283aef0a00cf0ffe893b",
+    "url": "/shared/script/script.js"
+  }
+]);
