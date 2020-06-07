de1a97ef0f9e627704e8dce2681af2a9cd687c0a
digitalfuture <avorio@mail.ru>
2020-06-07 15:35:16 +0500

    Fixed

--- shared-src/script/moveCountryList.js
+++ shared-src/script/moveCountryList.js
@@ -1,6 +1,6 @@
 export default function moveCountryList() {
   const countryList = document.querySelector('country-list')
-  const container = document.querySelector('.td-container tdc-content-wrap')
+  const container = document.querySelector('.td-container .tdc-content-wrap')
 
   container.appendChild(countryList)
 }
